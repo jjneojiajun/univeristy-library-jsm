4adc9b17191b7d3d2c8ee8c80edef22fa9e8039d
jjneojiajun <rockjj93@gmail.com>
2025-01-23 18:04:42 +0800

    complete setup upstash redis

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+const Page = () => {
+  return (
+    <main className="root-container flex min-h-screen flex-col items-center justify-center">
+      <h1 className="font-bebas-neue text-5xl font-bold text-light-100">Whoa, Slow Down There, Speedy!</h1>
+      <p className="mt-3 max-w-xl text-center text-light-400">
+        Take a moment to slow down and enjoy your visit! We know it’s tempting to scroll quickly, but there’s so much to
+        explore here. Whether it’s discovering new content, diving into details, or simply soaking up the experience,
+        giving yourself a little extra time ensures you won’t miss a thing. Relax, browse at your own pace, and make the
+        most of your time here.
+      </p>
+    </main>
+  );
+};
+
+export default Page;
---
+++ components/ImageUpload.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import Image from 'next/image';
+import * as React from 'react';
+import config from '@/lib/config';
+import { IKImage, ImageKitProvider, IKUpload } from 'imagekitio-next';
+import { useRef, useState } from 'react';
+import { useToast } from '@/hooks/use-toast';
+
+const {
+  env: {
+    imagekit: { publicKey, urlEndpoint },
+  },
+} = config;
+
+const authenticator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+
+      throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+    }
+
+    const data = await response.json();
+
+    const { signature, expire, token } = data;
+
+    return { token, expire, signature };
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+};
+
+const ImageUpload = ({ onFileChange }: { onFileChange: (filePath: string) => void }) => {
+  const ikUploadRef = useRef(null);
+  const [file, setFile] = useState<{ filePath: string } | null>(null);
+  const { toast } = useToast();
+
+  const onError = (error: any) => {
+    console.log(error);
+
+    toast({
+      title: 'Error Uploading Image',
+      description: 'Your image could not be uploaded. Please Try again.',
+      variant: 'destructive',
+    });
+  };
+  const onSuccess = (res: any) => {
+    setFile(res);
+    onFileChange(res.filePath);
+
+    toast({
+      title: 'Image Uploaded Successfully',
+      description: `${res.filePath} uploaded successfully!`,
+    });
+  };
+
+  return (
+    <ImageKitProvider publicKey={publicKey} urlEndpoint={urlEndpoint} authenticator={authenticator}>
+      <IKUpload
+        className="hidden"
+        ref={ikUploadRef}
+        onError={onError}
+        onSuccess={onSuccess}
+        fileName="test-upload.png"
+      />
+
+      <button
+        className="upload-btn"
+        onClick={(e) => {
+          e.preventDefault();
+
+          if (ikUploadRef.current) {
+            //@ts-ignore
+            ikUploadRef.current?.click();
+          }
+        }}
+      >
+        <Image src="/icons/upload.svg" alt="upload-icon" width={20} height={20} className="object-contain" />
+
+        <p className="text-base text-light-100">Upload a File</p>
+
+        {file && <p className="upload-filename">{file.filePath}</p>}
+      </button>
+
+      {file && <IKImage alt={file.filePath} path={file.filePath} width={500} height={300} />}
+    </ImageKitProvider>
+  );
+};
+
+export default ImageUpload;
